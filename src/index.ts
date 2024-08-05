598d1c30eb860ee54e20c0c0873bdc6437ca49dc
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-08-05 22:50:09 +0530

    backend setup

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/test', async (req: Request, res: Response) => {
+  res.json({ message: 'Hello!' });
+});
+
+app.listen(5050, () => {
+  console.log(`Server Listening to port: ${5050}`);
+});
