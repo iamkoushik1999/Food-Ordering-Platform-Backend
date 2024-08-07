d0eca99c4ac5f27466193c82d4353de8de927e47
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-08-07 23:14:49 +0530

    Fix: database setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/database';
+connectDB();
 
 const app = express();
 
